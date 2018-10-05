7dd19b8844110a244c9635c47fb0b0e13753c379
Lucas Giancola <lgiancol@uoguelph.ca>
2018-10-05 13:25:50 -0400

    Set up a system where a user can press 'e' on an item and if it is interactable it will do what it needs to do

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+	public Camera cam;
+	public float radius = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("e")) {
+			Ray ray = new Ray(cam.transform.position, cam.transform.forward);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit, radius)) {
+				Interactable item = hit.collider.GetComponent<Interactable>();
+
+				if(item != null) {
+					item.onInteract();
+				} else {
+					Debug.Log("Hit something that isn't interactive");
+				}
+			} else {
+				Debug.Log("Nothing in range");
+			}
+		}
+	}
+}
