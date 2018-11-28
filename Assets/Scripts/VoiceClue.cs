608361a0e0e11335ceb58e3da706ece68c64905e
Haroons23 <haroons23@hotmail.com>
2018-11-28 14:26:24 -0500

    adding riddle puzzle

---
+++ Assets/Scripts/VoiceClue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceClue : InteractableItem {
+
+    //public AudioClip audioCode;
+    public AudioSource sourceCode;
+
+    public void Start()
+    {
+        sourceCode = GetComponent<AudioSource>();
+    }
+
+    public override void onInteract()
+	{
+        sourceCode.Play();
+	}
+}
